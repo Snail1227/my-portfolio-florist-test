18850cda257f5150862ea0b94f9065418a7b17c1
Snail1227 <garrishtyrkin@gmail.com>
2024-11-19 16:21:02 -0500

    updated images

--- src/images.jsx
+++ src/images.jsx
@@ -6,12 +6,15 @@ const Gallery = () => {
   useEffect(() => {
     const fetchImages = async () => {
       try {
+        const folderName = "florist-portfolio"; // Replace with your folder or tag name
         const response = await fetch(
-          `https://res.cloudinary.com/dux7p9r1o/image/list/florist-portfolio.json`
+          `https://res.cloudinary.com/dux7p9r1o/resources/image/tags/${folderName}`
         );
+
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
+
         const data = await response.json();
         setImages(data.resources); // Set image resources from the response
       } catch (error) {
@@ -29,7 +32,7 @@ const Gallery = () => {
         {images.map((image) => (
           <img
             key={image.public_id}
-            src={`https://res.cloudinary.com/dux7p9r1o/image/upload/${image.public_id}`}
+            src={`https://res.cloudinary.com/dux7p9r1o/image/upload/v${image.version}/${image.public_id}.jpg`} // Updated URL format
             alt={image.public_id}
             style={{ width: "200px", margin: "10px" }}
           />
