77c1dff4b030ebb494c73b4a5d543f4783a7e6ea
Snail1227 <garrishtyrkin@gmail.com>
2024-11-19 18:17:35 -0500

    updated images

--- src/API.jsx
+++ src/API.jsx
@@ -8,7 +8,7 @@ const APIimages = () => {
   
   // Use this sample image or upload your own via the Media Explorer
   const img = cld
-        .image('cld-sample-5')
+        .image('florist-portfolio/1_1')
         .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
         .quality('auto')
         .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
