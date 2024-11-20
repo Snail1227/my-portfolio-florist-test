f65b435c74d48cbd8e8bc24aac3b8a8bccfe0e59
Snail1227 <garrishtyrkin@gmail.com>
2024-11-20 18:33:14 -0500

    updated images

--- src/API.jsx
+++ src/API.jsx
@@ -2,12 +2,13 @@
 const APIimages = () => {
 
   const cloudName = 'dux7p9r1o'; // Replace with your Cloudinary cloud name
-  const transformations = 'c_fill,q_auto,f_auto';
+  const transformations = 'h_400,w_400,c_fill,q_auto,f_auto';
 
   const imageNames = [
     'Fall_Flowers_NYC_w2amyp',
     'image_93884367-e24d-4537-b270-4ab3ec3f49a3_grande_wkcndk',
-    '167398L__04164_tweahv'
+    '167398L__04164_tweahv',
+    'prettiest-flowers-painted-tongue-flower-lead-getty-1123-00763085ad384a9b9bf1f5cc81bee390_jdvtme.jpg'
   ];
 
   const generateImageUrls = (cloudName, transformations, imageNames) => {
@@ -20,13 +21,13 @@ const APIimages = () => {
   const transformedUrls = generateImageUrls(cloudName, transformations, imageNames);
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'center' }}>
       {transformedUrls.map((url, index) => (
         <img
           key={index}
           src={url}
           alt={`Optimized Image ${index + 1}`}
-          style={{ width: 'auto', height: 'auto', objectFit: 'cover' }}
+          className="hover-image"
         />
       ))}
     </div>
